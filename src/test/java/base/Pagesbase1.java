dabdd07adc15b105437e3c078c15d95326585bb1
unknown <you@example.com>
2020-04-05 18:13:01 +0530

    commit

--- src/test/java/base/Pagesbase1.java
+++ src/test/java/base/Pagesbase1.java
@@ -1,11 +1,15 @@
 package base;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
@@ -19,13 +23,20 @@ public class Pagesbase1{
 	public static Logger log = Logger.getLogger("devpinoyLogger");
 	public static Properties or = new Properties();
 	public static Properties config = new Properties();
+	DesiredCapabilities cap= new DesiredCapabilities();
 	
-	public Pagesbase1()
+	public Pagesbase1() 
 	{
 		if(driver==null)
 		{
+			cap.setBrowserName("chrome");
 			WebDriverManager.firefoxdriver().setup();
-			driver =new FirefoxDriver();
+			try {
+				driver =new RemoteWebDriver(new URL("http://192.168.1.11:8078/wd/hub"),cap);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			driver.get("https://www.naukri.com/");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
